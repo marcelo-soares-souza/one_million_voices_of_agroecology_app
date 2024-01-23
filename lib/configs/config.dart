30fdd914cfa76ee4249d7956faca67a8287b4657
Marcelo Soares Souza <marcelo@agroecologymap.org>
2024-01-23 12:47:08 -0300

    feat: update app theme and configuration

--- lib/configs/config.dart
+++ lib/configs/config.dart
@@ -1,3 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
 class Config {
+  static const String title = 'One Million Voices of Agroecology';
   static const String omvUrl = 'onemillionvoices.agroecologymap.org';
+
+  static final _colorScheme = ColorScheme.fromSeed(
+    brightness: Brightness.dark,
+    seedColor: const Color.fromARGB(255, 69, 92, 76),
+    background: const Color.fromARGB(255, 24, 34, 27),
+  );
+
+  static final mainTheme = ThemeData().copyWith(
+    scaffoldBackgroundColor: _colorScheme.background,
+    colorScheme: _colorScheme,
+    textTheme: GoogleFonts.ubuntuCondensedTextTheme().copyWith(
+      titleSmall: GoogleFonts.ubuntuCondensed(
+        fontWeight: FontWeight.bold,
+      ),
+      titleMedium: GoogleFonts.ubuntuCondensed(
+        fontWeight: FontWeight.bold,
+      ),
+      titleLarge: GoogleFonts.ubuntuCondensed(
+        fontWeight: FontWeight.bold,
+      ),
+    ),
+  );
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
+import 'package:one_million_voices_of_agroecology_app/configs/config.dart';
 import 'package:one_million_voices_of_agroecology_app/screens/home.dart';
-
-final colorScheme = ColorScheme.fromSeed(
-  brightness: Brightness.dark,
-  seedColor: const Color.fromARGB(255, 69, 92, 76),
-  background: const Color.fromARGB(255, 24, 34, 27),
-);
-
-final theme = ThemeData().copyWith(
-  scaffoldBackgroundColor: colorScheme.background,
-  colorScheme: colorScheme,
-  textTheme: GoogleFonts.ubuntuCondensedTextTheme().copyWith(
-    titleSmall: GoogleFonts.ubuntuCondensed(
-      fontWeight: FontWeight.bold,
-    ),
-    titleMedium: GoogleFonts.ubuntuCondensed(
-      fontWeight: FontWeight.bold,
-    ),
-    titleLarge: GoogleFonts.ubuntuCondensed(
-      fontWeight: FontWeight.bold,
-    ),
-  ),
-);
 
 void main() {
   runApp(const OneMillionVoicesofAgroecologyApp());
@@ -35,8 +13,8 @@ class OneMillionVoicesofAgroecologyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'One Million Voices of Agroecology',
-      theme: theme,
+      title: Config.title,
+      theme: Config.mainTheme,
       home: const HomeScreen(),
     );
   }
