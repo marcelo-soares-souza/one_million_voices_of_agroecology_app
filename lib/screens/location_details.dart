6c68266340704d0f0788253f8d41f1360acc8cbf
Marcelo Soares Souza <marcelo@agroecologymap.org>
2024-01-29 16:15:42 -0300

    feat: added location gallery

--- lib/screens/location_details.dart
+++ lib/screens/location_details.dart
@@ -1,7 +1,10 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:one_million_voices_of_agroecology_app/models/gallery_item.dart';
 
 import 'package:one_million_voices_of_agroecology_app/models/location.dart';
+import 'package:one_million_voices_of_agroecology_app/services/location_service.dart';
 import 'package:one_million_voices_of_agroecology_app/widgets/text_block_widget.dart';
 
 class LocationDetailsScreen extends StatefulWidget {
@@ -17,6 +20,13 @@ class LocationDetailsScreen extends StatefulWidget {
 
 class _LocationDetailsScreen extends State<LocationDetailsScreen> {
   bool isFavorite = false;
+  int _selectedPageIndex = 0;
+  late List<GalleryItem> _gallery;
+
+  void _retrieveGallery() async {
+    _gallery = await LocationService.retrieveLocationGallery(
+        widget.location.id.toString());
+  }
 
   void _setFavorite(Location location) {
     setState(() {
@@ -24,64 +34,157 @@ class _LocationDetailsScreen extends State<LocationDetailsScreen> {
     });
   }
 
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _retrieveGallery();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.location.name),
-        actions: [
-          IconButton(
-            onPressed: () {
-              _setFavorite(widget.location);
-            },
-            icon: AnimatedSwitcher(
-              duration: const Duration(milliseconds: 500),
-              child: Icon(isFavorite ? Icons.star : Icons.star_border,
-                  key: ValueKey(isFavorite)),
-              transitionBuilder: (child, animation) => RotationTransition(
-                turns: animation,
-                child: child,
-              ),
-            ),
-          )
-        ],
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            CachedNetworkImage(
-              height: 250,
-              width: double.infinity,
-              fit: BoxFit.cover,
-              placeholder: (context, url) => const Center(
-                child: SizedBox(
-                  width: 30.0,
-                  height: 30.0,
-                  child: CircularProgressIndicator(),
+        appBar: AppBar(
+          title: Text(widget.location.name),
+          actions: [
+            IconButton(
+              onPressed: () {
+                _setFavorite(widget.location);
+              },
+              icon: AnimatedSwitcher(
+                duration: const Duration(milliseconds: 500),
+                child: Icon(isFavorite ? Icons.star : Icons.star_border,
+                    key: ValueKey(isFavorite)),
+                transitionBuilder: (child, animation) => RotationTransition(
+                  turns: animation,
+                  child: child,
                 ),
               ),
-              imageUrl: widget.location.imageUrl,
-            ),
-            TextBlockWidget(
-              label: 'Description',
-              value: widget.location.description,
-            ),
-            TextBlockWidget(
-              label: 'Country',
-              value: widget.location.country,
-            ),
-            TextBlockWidget(
-              label: 'Farm and Farming System',
-              value:
-                  '${widget.location.farmAndFarmingSystem} - ${widget.location.farmAndFarmingSystemComplement}',
-            ),
-            TextBlockWidget(
-              label: 'Responsible for Information',
-              value: widget.location.responsibleForInformation,
-            ),
+            )
           ],
         ),
-      ),
-    );
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              if (_selectedPageIndex == 0) ...[
+                CachedNetworkImage(
+                  height: 250,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                  placeholder: (context, url) => const Center(
+                    child: SizedBox(
+                      width: 30.0,
+                      height: 30.0,
+                      child: CircularProgressIndicator(),
+                    ),
+                  ),
+                  imageUrl: widget.location.imageUrl,
+                ),
+                TextBlockWidget(
+                  label: 'Description',
+                  value: widget.location.description,
+                ),
+                TextBlockWidget(
+                  label: 'Country',
+                  value: widget.location.country,
+                ),
+                TextBlockWidget(
+                  label: 'Farm and Farming System',
+                  value:
+                      '${widget.location.farmAndFarmingSystem} - ${widget.location.farmAndFarmingSystemComplement}',
+                ),
+                TextBlockWidget(
+                  label: 'Responsible for Information',
+                  value: widget.location.responsibleForInformation,
+                ),
+              ] else if (_selectedPageIndex == 1) ...[
+                //
+                // Gallery
+                //
+                if (_gallery.isEmpty)
+                  Center(
+                    child: Text(
+                      'No items',
+                      style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                            color: Theme.of(context).colorScheme.secondary,
+                          ),
+                    ),
+                  )
+                else
+                  for (final i in _gallery) ...[
+                    Stack(
+                      children: [
+                        CachedNetworkImage(
+                          height: 300,
+                          width: double.infinity,
+                          fit: BoxFit.cover,
+                          placeholder: (context, url) => const Center(
+                              child: SizedBox(
+                            width: 30.0,
+                            height: 30.0,
+                            child: CircularProgressIndicator(),
+                          )),
+                          imageUrl: i.imageUrl,
+                        ),
+                        if (i.description.length > 5)
+                          Positioned(
+                            bottom: 0,
+                            left: 0,
+                            right: 0,
+                            child: Container(
+                              color: Colors.black54,
+                              padding: const EdgeInsets.symmetric(
+                                vertical: 6,
+                                horizontal: 44,
+                              ),
+                              child: Column(
+                                children: [
+                                  Text(
+                                    i.description,
+                                    maxLines: 2,
+                                    textAlign: TextAlign.center,
+                                    softWrap: true,
+                                    overflow: TextOverflow.ellipsis,
+                                    style: const TextStyle(
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          )
+                      ],
+                    ),
+                    const SizedBox(height: 20),
+                  ]
+              ]
+            ],
+          ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: Theme.of(context).colorScheme.primaryContainer,
+          type: BottomNavigationBarType.fixed,
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          onTap: _selectPage,
+          currentIndex: _selectedPageIndex,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(FontAwesomeIcons.locationDot),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(FontAwesomeIcons.photoFilm),
+              label: 'Gallery',
+            ),
+          ],
+        ));
   }
 }
--- lib/services/location_service.dart
+++ lib/services/location_service.dart
@@ -2,6 +2,7 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 import 'package:one_million_voices_of_agroecology_app/configs/config.dart';
+import 'package:one_million_voices_of_agroecology_app/models/gallery_item.dart';
 import 'package:one_million_voices_of_agroecology_app/models/location.dart';
 
 class LocationService {
@@ -15,5 +16,21 @@ class LocationService {
     }
 
     return locations;
+  }
+
+  static Future<List<GalleryItem>> retrieveLocationGallery(String id) async {
+    final List<GalleryItem> gallery = [];
+
+    final res =
+        await http.get(Uri.https(Config.omvUrl, '/locations/$id/gallery.json'));
+
+    var data = json.decode(res.body.toString());
+
+    if (res.body.length > 14) {
+      for (final item in data['gallery']) {
+        gallery.add(GalleryItem.fromJson(item));
+      }
+    }
+    return gallery;
   }
 }
